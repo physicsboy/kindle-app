b5c00dbae486c7722caeeae4a186b600394df3d4
physicsboy <amhighton@gmail.com>
2026-03-18 18:04:20 +0000

    updates

--- functions/shopping/delete.js
+++ functions/shopping/delete.js
@@ -1,10 +1,24 @@
+// export async function onRequestDelete({ request }) {
+//   const url = new URL(request.url);
+//   const id = url.searchParams.get("id");
+
+//   console.log("Delete:", id);
+
+//   // TODO: delete from DB
+
+//   return fetch(new Request(new URL("/shopping/list", request.url)));
+// }
+
 export async function onRequestDelete({ request }) {
   const url = new URL(request.url);
   const id = url.searchParams.get("id");
 
-  console.log("Delete:", id);
+  console.log("Deleting ID:", id);
 
-  // TODO: delete from DB
-
-  return fetch(new Request(new URL("/shopping/list", request.url)));
+  return new Response("", {
+    status: 200,
+    headers: { 
+      "HX-Trigger": "reloadList" 
+    }
+  });
 }
